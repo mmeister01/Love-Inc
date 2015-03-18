46774a9c56434eea87f436967bc82ccd048b7a89
Alex Johnson <mrjohnsonalex@gmail.com>
2015-03-18 13:01:29 -0400

    got footer to be fixed

--- index.php
+++ index.php
@@ -48,163 +48,164 @@
 
 	<!-- header start -->
 	<!-- ================ -->
-	<header class="header fixed clearfix navbar navbar-fixed-top">
-		<div class="container">
-			<div class="row">
-				<div class="col-md-4">
-
-					<!-- header-left start -->
-					<!-- ================ -->
-					<div class="header-left clearfix">
-
-						<!-- logo -->
-						<div class="logo smooth-scroll">
-							<a href="#banner"><img id="logo" src="images/logo.png"
-								alt="Love INC"></a>
-						</div>
-
-						<!-- name-and-slogan -->
-						<div class="site-name-and-slogan smooth-scroll">
-							<div class="site-name">
-								<a href="#banner">Love INC</a>
-							</div>
-							<div class="site-slogan">Love In The Name of Christ</div>
-						</div>
-
-					</div>
-					<!-- header-left end -->
-
-				</div>
-				<div class="col-md-8">
-
-					<!-- header-right start -->
-					<!-- ================ -->
-					<div class="header-right clearfix">
-
-						<!-- main-navigation start -->
-						<!-- ================ -->
-						<div class="main-navigation animated">
-
-							<!-- navbar start -->
-							<!-- ================ -->
-							<nav class="navbar navbar-default" role="navigation">
-								<div class="container-fluid">
-
-									<!-- Toggle get grouped for better mobile display -->
-									<div class="navbar-header">
-										<button type="button" class="navbar-toggle"
-											data-toggle="collapse" data-target="#navbar-collapse-1">
-											<span class="sr-only">Toggle navigation</span> <span
-												class="icon-bar"></span> <span class="icon-bar"></span> <span
-												class="icon-bar"></span>
-										</button>
-									</div>
-
-									<!-- Collect the nav links, forms, and other content for toggling -->
-									<div class="collapse navbar-collapse scrollspy smooth-scroll"
-										id="navbar-collapse-1">
-										<ul class="nav navbar-nav navbar-right">
-											<li class="active"><a href="#banner">Home</a></li>
-											<li><a href="#about">About</a></li>
-											<li><a href="#contact">Calendar</a></li>
-											<li><a href="#calendar">Contact</a></li>
-										</ul>
-									</div>
-
-								</div>
-							</nav>
-							<!-- navbar end -->
-
-						</div>
-						<!-- main-navigation end -->
-
-					</div>
-					<!-- header-right end -->
-
-				</div>
-			</div>
-		</div>
-	</header>
-	<!-- header end -->
-
-	<!-- banner start -->
-	<!-- ================ -->
-	<div id="banner" class="banner">
-		<div class="banner-image"></div>
-		<div class="banner-caption">
+	
+		<header class="header fixed clearfix navbar navbar-fixed-top">
 			<div class="container">
 				<div class="row">
-					<div class="col-md-8 col-md-offset-2 object-non-visible"
-						data-animation-effect="fadeIn">
-						<h1 class="text-center"></h1>
-						<p class="lead text-center"></p>
+					<div class="col-md-4">
+
+						<!-- header-left start -->
+						<!-- ================ -->
+						<div class="header-left clearfix">
+
+							<!-- logo -->
+							<div class="logo smooth-scroll">
+								<a href="#banner"><img id="logo" src="images/logo.png"
+									alt="Love INC"></a>
+							</div>
+
+							<!-- name-and-slogan -->
+							<div class="site-name-and-slogan smooth-scroll">
+								<div class="site-name">
+									<a href="#banner">Love INC</a>
+								</div>
+								<div class="site-slogan">Love In The Name of Christ</div>
+							</div>
+
+						</div>
+						<!-- header-left end -->
+
+					</div>
+					<div class="col-md-8">
+
+						<!-- header-right start -->
+						<!-- ================ -->
+						<div class="header-right clearfix">
+
+							<!-- main-navigation start -->
+							<!-- ================ -->
+							<div class="main-navigation animated">
+
+								<!-- navbar start -->
+								<!-- ================ -->
+								<nav class="navbar navbar-default" role="navigation">
+									<div class="container-fluid">
+
+										<!-- Toggle get grouped for better mobile display -->
+										<div class="navbar-header">
+											<button type="button" class="navbar-toggle"
+												data-toggle="collapse" data-target="#navbar-collapse-1">
+												<span class="sr-only">Toggle navigation</span> <span
+													class="icon-bar"></span> <span class="icon-bar"></span> <span
+													class="icon-bar"></span>
+											</button>
+										</div>
+
+										<!-- Collect the nav links, forms, and other content for toggling -->
+										<div class="collapse navbar-collapse scrollspy smooth-scroll"
+											id="navbar-collapse-1">
+											<ul class="nav navbar-nav navbar-right">
+												<li class="active"><a href="#banner">Home</a></li>
+												<li><a href="#about">About</a></li>
+												<li><a href="#contact">Calendar</a></li>
+												<li><a href="#calendar">Contact</a></li>
+											</ul>
+										</div>
+
+									</div>
+								</nav>
+								<!-- navbar end -->
+
+							</div>
+							<!-- main-navigation end -->
+
+						</div>
+						<!-- header-right end -->
+
+					</div>
+				</div>
+			</div>
+		</header>
+		<!-- header end -->
+
+		<!-- banner start -->
+		<!-- ================ -->
+		<div id="banner" class="banner">
+			<div class="banner-image"></div>
+			<div class="banner-caption">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-8 col-md-offset-2 object-non-visible"
+							data-animation-effect="fadeIn">
+							<h1 class="text-center"></h1>
+							<p class="lead text-center"></p>
+						</div>
 					</div>
 				</div>
 			</div>
 		</div>
-	</div>
-	<!-- banner end -->
-	<div class="container content">
-		<section id="about">Lorem ipsum dolor sit amet, consectetur adipiscing
-			elit. Praesent ac quam tortor. Vivamus luctus dui sed arcu volutpat,
-			vitae mattis neque viverra. Nulla facilisis nibh sit amet finibus
-			interdum. In sed ante justo. Duis et nibh ac nibh tristique convallis
-			ac vitae risus. Suspendisse tincidunt tortor suscipit nisl sagittis
-			volutpat. Suspendisse vestibulum risus ut orci interdum fermentum.
-			Nunc elementum id tellus vitae ultricies. Sed vitae leo tincidunt,
-			consectetur urna sed, viverra felis. Nam maximus venenatis justo,
-			quis viverra nisi vestibulum eget. Morbi sit amet lectus et massa
-			placerat porttitor. Donec a dui euismod nibh suscipit rutrum. Mauris
-			ultrices id nunc at venenatis. Nulla nibh tellus, euismod at purus a,
-			rhoncus dapibus urna. Quisque eget dictum nulla, eu ornare neque.
+		<!-- banner end -->
 
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
-			nec vestibulum justo. Aenean rhoncus faucibus posuere. Cras ut lorem
-			in mauris gravida lobortis. Nunc ut odio turpis. Nam finibus dictum
-			leo at tempus. In quis pulvinar nibh. Mauris egestas nibh sit amet
-			nulla vehicula, eget ullamcorper mi ultricies. Class aptent taciti
-			sociosqu ad litora torquent per conubia nostra, per inceptos
-			himenaeos. Proin semper, nisi sit amet tincidunt pretium, enim nisi
-			molestie dui, et luctus nunc enim ut ante. Vivamus porta, lacus id
-			sagittis sollicitudin, nibh nibh laoreet elit, fermentum placerat
-			diam ante tempor nisi. Suspendisse blandit a tortor eget blandit.
-			Morbi eu metus mollis, feugiat massa eget, elementum sem.
-			Pellentesque lacinia vestibulum nibh eget faucibus. Sed odio arcu,
-			volutpat nec tellus condimentum, dapibus laoreet nunc. Donec posuere
-			elit eu odio sollicitudin, rutrum porta quam hendrerit. In nec auctor
-			lectus, ut fermentum leo. Nunc vitae porttitor nibh. Donec a rhoncus
-			ligula. Donec aliquam faucibus velit, non efficitur quam sagittis eu.
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
-			egestas ligula nec ante vestibulum, et semper ex aliquet. Duis
-			aliquet urna quis sapien congue, eget dictum odio accumsan. Integer
-			eget purus tincidunt, vestibulum nibh sed, sodales neque. Curabitur
-			in ultricies sapien, id blandit lacus. Maecenas ut ante malesuada
-			eros ultricies aliquam in ut purus. Sed nec sem ligula. Aenean
-			faucibus tempus leo, sed tristique nulla. Mauris dolor odio, pulvinar
-			a purus at, fermentum dapibus mi. Ut vel accumsan dui, sed tincidunt
-			velit. Class aptent taciti sociosqu ad litora torquent per conubia
-			nostra, per inceptos himenaeos. Suspendisse vitae viverra nunc.
-			Integer molestie arcu elit, id vulputate nisl lacinia a. Proin turpis
-			erat, sodales eu vehicula at, vestibulum vitae magna. Vestibulum elit
-			sem, cursus et vestibulum vitae, finibus eget tellus. Vivamus ut
-			accumsan massa. Nunc consequat felis a arcu sagittis lacinia.
-			Pellentesque in condimentum eros, eget maximus diam. Proin pulvinar
-			ex ipsum, sed ornare massa ultrices quis. Duis imperdiet, diam vel
-			vestibulum ultricies, ligula ligula pretium dolor, a iaculis urna
-			nisl et eros. Nullam mollis euismod velit, vitae lacinia purus
-			interdum sit amet. Aenean condimentum mauris vitae orci vestibulum
-			pellentesque. Phasellus congue sapien nec dolor consequat feugiat.
-			Phasellus sit amet massa aliquam, tristique nisl at, molestie augue.
-			Etiam eu ante ut nibh sodales aliquet quis non quam. Nunc ut justo
-			sed erat cursus vehicula.</section>
-	</div>
-	
+		<div class="container wrap">
+			<section id="about">Lorem ipsum dolor sit amet, consectetur
+				adipiscing elit. Praesent ac quam tortor. Vivamus luctus dui sed
+				arcu volutpat, vitae mattis neque viverra. Nulla facilisis nibh sit
+				amet finibus interdum. In sed ante justo. Duis et nibh ac nibh
+				tristique convallis ac vitae risus. Suspendisse tincidunt tortor
+				suscipit nisl sagittis volutpat. Suspendisse vestibulum risus ut
+				orci interdum fermentum. Nunc elementum id tellus vitae ultricies.
+				Sed vitae leo tincidunt, consectetur urna sed, viverra felis. Nam
+				maximus venenatis justo, quis viverra nisi vestibulum eget. Morbi
+				sit amet lectus et massa placerat porttitor. Donec a dui euismod
+				nibh suscipit rutrum. Mauris ultrices id nunc at venenatis. Nulla
+				nibh tellus, euismod at purus a, rhoncus dapibus urna. Quisque eget
+				dictum nulla, eu ornare neque. Lorem ipsum dolor sit amet,
+				consectetur adipiscing elit. Curabitur nec vestibulum justo. Aenean
+				rhoncus faucibus posuere. Cras ut lorem in mauris gravida lobortis.
+				Nunc ut odio turpis. Nam finibus dictum leo at tempus. In quis
+				pulvinar nibh. Mauris egestas nibh sit amet nulla vehicula, eget
+				ullamcorper mi ultricies. Class aptent taciti sociosqu ad litora
+				torquent per conubia nostra, per inceptos himenaeos. Proin semper,
+				nisi sit amet tincidunt pretium, enim nisi molestie dui, et luctus
+				nunc enim ut ante. Vivamus porta, lacus id sagittis sollicitudin,
+				nibh nibh laoreet elit, fermentum placerat diam ante tempor nisi.
+				Suspendisse blandit a tortor eget blandit. Morbi eu metus mollis,
+				feugiat massa eget, elementum sem. Pellentesque lacinia vestibulum
+				nibh eget faucibus. Sed odio arcu, volutpat nec tellus condimentum,
+				dapibus laoreet nunc. Donec posuere elit eu odio sollicitudin,
+				rutrum porta quam hendrerit. In nec auctor lectus, ut fermentum leo.
+				Nunc vitae porttitor nibh. Donec a rhoncus ligula. Donec aliquam
+				faucibus velit, non efficitur quam sagittis eu. Lorem ipsum dolor
+				sit amet, consectetur adipiscing elit. Mauris egestas ligula nec
+				ante vestibulum, et semper ex aliquet. Duis aliquet urna quis sapien
+				congue, eget dictum odio accumsan. Integer eget purus tincidunt,
+				vestibulum nibh sed, sodales neque. Curabitur in ultricies sapien,
+				id blandit lacus. Maecenas ut ante malesuada eros ultricies aliquam
+				in ut purus. Sed nec sem ligula. Aenean faucibus tempus leo, sed
+				tristique nulla. Mauris dolor odio, pulvinar a purus at, fermentum
+				dapibus mi. Ut vel accumsan dui, sed tincidunt velit. Class aptent
+				taciti sociosqu ad litora torquent per conubia nostra, per inceptos
+				himenaeos. Suspendisse vitae viverra nunc. Integer molestie arcu
+				elit, id vulputate nisl lacinia a. Proin turpis erat, sodales eu
+				vehicula at, vestibulum vitae magna. Vestibulum elit sem, cursus et
+				vestibulum vitae, finibus eget tellus. Vivamus ut accumsan massa.
+				Nunc consequat felis a arcu sagittis lacinia. Pellentesque in
+				condimentum eros, eget maximus diam. Proin pulvinar ex ipsum, sed
+				ornare massa ultrices quis. Duis imperdiet, diam vel vestibulum
+				ultricies, ligula ligula pretium dolor, a iaculis urna nisl et eros.
+				Nullam mollis euismod velit, vitae lacinia purus interdum sit amet.
+				Aenean condimentum mauris vitae orci vestibulum pellentesque.
+				Phasellus congue sapien nec dolor consequat feugiat. Phasellus sit
+				amet massa aliquam, tristique nisl at, molestie augue. Etiam eu ante
+				ut nibh sodales aliquet quis non quam. Nunc ut justo sed erat cursus
+				vehicula.</section>
+		</div>
 	<footer class="footer">
-	<div class="container">
-	<p class="text-muted">
+		<div class="container">
+			<p class="text-muted">
 	Copyright &copy; Love Inc <?php echo date("Y"); ?>
 	</p>
-	</div>
+		</div>
 	</footer>
 
 	<!-- JavaScript files placed at the end of the document so the pages load faster
