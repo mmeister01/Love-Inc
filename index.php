2e448756ef7f672d0d2378e0c252a6640fe12981
Alex Johnson <alex@archlinux.local>
2015-03-18 12:50:24 -0400

    added about us page and started adding footer

--- index.php
+++ index.php
@@ -3,165 +3,234 @@
 <!--[if !IE]><!-->
 <html lang="en">
 <!--<![endif]-->
-	<head>
-		<meta charset="utf-8">
-		<title>Love INC</title>
-		<meta name="description" content="Love In The Name of Christ (Love INC)">
-		<meta name="author" content="Alex Johnson, David Poling, and Melanie Meister">
+<head>
+<meta charset="utf-8">
+<title>Love INC</title>
+<meta name="description" content="Love In The Name of Christ (Love INC)">
+<meta name="author"
+	content="Alex Johnson, David Poling, and Melanie Meister">
 
-		<!-- Mobile Meta -->
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<!-- Mobile Meta -->
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-		<!-- Favicon -->
-		<link rel="shortcut icon" href="images/favicon.ico">
+<!-- Favicon -->
+<link rel="shortcut icon" href="images/favicon.ico">
 
-		<!-- Web Fonts -->
-		<link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,700italic,400,700,300&amp;subset=latin,latin-ext' rel='stylesheet' type='text/css'>
-		<link href='http://fonts.googleapis.com/css?family=Raleway:700,400,300' rel='stylesheet' type='text/css'>
+<!-- Web Fonts -->
+<link
+	href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,700italic,400,700,300&amp;subset=latin,latin-ext'
+	rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Raleway:700,400,300'
+	rel='stylesheet' type='text/css'>
 
-		<!-- Bootstrap core CSS -->
-		<link href="css/bootstrap.min.css" rel="stylesheet">
+<!-- Bootstrap core CSS -->
+<link href="css/bootstrap.min.css" rel="stylesheet">
 
-		<!-- Font Awesome CSS -->
-		<link href="fonts/font-awesome/css/font-awesome.css" rel="stylesheet">
+<!-- Font Awesome CSS -->
+<link href="fonts/font-awesome/css/font-awesome.css" rel="stylesheet">
 
-		<!-- Plugins -->
-		<link href="css/animations.css" rel="stylesheet">
+<!-- Plugins -->
+<link href="css/animations.css" rel="stylesheet">
 
-		<!-- Worthy core CSS file -->
-		<link href="css/style.css" rel="stylesheet">
+<!-- Worthy core CSS file -->
+<link href="css/style.css" rel="stylesheet">
 
-		<!-- Custom css --> 
-		<link href="css/custom.css" rel="stylesheet">
-	</head>
+<!-- Custom css -->
+<link href="css/custom.css" rel="stylesheet">
+</head>
 
-	<body class="no-trans">
-		<!-- scrollToTop -->
-		<!-- ================ -->
-		<div class="scrollToTop"><i class="icon-up-open-big"></i></div>
+<body class="no-trans">
+	<!-- scrollToTop -->
+	<!-- ================ -->
+	<div class="scrollToTop">
+		<i class="icon-up-open-big"></i>
+	</div>
 
-		<!-- header start -->
-		<!-- ================ --> 
-		<header class="header fixed clearfix navbar navbar-fixed-top">
-			<div class="container">
-				<div class="row">
-					<div class="col-md-4">
+	<!-- header start -->
+	<!-- ================ -->
+	<header class="header fixed clearfix navbar navbar-fixed-top">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-4">
 
-						<!-- header-left start -->
-						<!-- ================ -->
-						<div class="header-left clearfix">
+					<!-- header-left start -->
+					<!-- ================ -->
+					<div class="header-left clearfix">
 
-							<!-- logo -->
-							<div class="logo smooth-scroll">
-								<a href="#banner"><img id="logo" src="images/logo.png" alt="Love INC"></a>
-							</div>
-
-							<!-- name-and-slogan -->
-							<div class="site-name-and-slogan smooth-scroll">
-								<div class="site-name"><a href="#banner">Love INC</a></div>
-								<div class="site-slogan">Love In The Name of Christ</div>
-							</div>
-
+						<!-- logo -->
+						<div class="logo smooth-scroll">
+							<a href="#banner"><img id="logo" src="images/logo.png"
+								alt="Love INC"></a>
 						</div>
-						<!-- header-left end -->
+
+						<!-- name-and-slogan -->
+						<div class="site-name-and-slogan smooth-scroll">
+							<div class="site-name">
+								<a href="#banner">Love INC</a>
+							</div>
+							<div class="site-slogan">Love In The Name of Christ</div>
+						</div>
 
 					</div>
-					<div class="col-md-8">
+					<!-- header-left end -->
 
-						<!-- header-right start -->
+				</div>
+				<div class="col-md-8">
+
+					<!-- header-right start -->
+					<!-- ================ -->
+					<div class="header-right clearfix">
+
+						<!-- main-navigation start -->
 						<!-- ================ -->
-						<div class="header-right clearfix">
+						<div class="main-navigation animated">
 
-							<!-- main-navigation start -->
+							<!-- navbar start -->
 							<!-- ================ -->
-							<div class="main-navigation animated">
+							<nav class="navbar navbar-default" role="navigation">
+								<div class="container-fluid">
 
-								<!-- navbar start -->
-								<!-- ================ -->
-								<nav class="navbar navbar-default" role="navigation">
-									<div class="container-fluid">
-
-										<!-- Toggle get grouped for better mobile display -->
-										<div class="navbar-header">
-											<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse-1">
-												<span class="sr-only">Toggle navigation</span>
-												<span class="icon-bar"></span>
-												<span class="icon-bar"></span>
-												<span class="icon-bar"></span>
-											</button>
-										</div>
-
-										<!-- Collect the nav links, forms, and other content for toggling -->
-										<div class="collapse navbar-collapse scrollspy smooth-scroll" id="navbar-collapse-1">
-											<ul class="nav navbar-nav navbar-right">
-												<li class="active"><a href="#banner">Home</a></li>
-												<li><a href="#about">About</a></li>
-												<li><a href="#services">Services</a></li>
-												<li><a href="#portfolio">Portfolio</a></li>
-												<li><a href="#clients">Clients</a></li>
-												<li><a href="#contact">Calendar</a></li>
-                                                                                                <li><a href="#calendar">Contact</a></li>
-											</ul>
-										</div>
-
+									<!-- Toggle get grouped for better mobile display -->
+									<div class="navbar-header">
+										<button type="button" class="navbar-toggle"
+											data-toggle="collapse" data-target="#navbar-collapse-1">
+											<span class="sr-only">Toggle navigation</span> <span
+												class="icon-bar"></span> <span class="icon-bar"></span> <span
+												class="icon-bar"></span>
+										</button>
 									</div>
-								</nav>
-								<!-- navbar end -->
 
-							</div>
-							<!-- main-navigation end -->
+									<!-- Collect the nav links, forms, and other content for toggling -->
+									<div class="collapse navbar-collapse scrollspy smooth-scroll"
+										id="navbar-collapse-1">
+										<ul class="nav navbar-nav navbar-right">
+											<li class="active"><a href="#banner">Home</a></li>
+											<li><a href="#about">About</a></li>
+											<li><a href="#contact">Calendar</a></li>
+											<li><a href="#calendar">Contact</a></li>
+										</ul>
+									</div>
+
+								</div>
+							</nav>
+							<!-- navbar end -->
 
 						</div>
-						<!-- header-right end -->
+						<!-- main-navigation end -->
 
 					</div>
+					<!-- header-right end -->
+
 				</div>
 			</div>
-		</header>
-		<!-- header end -->
+		</div>
+	</header>
+	<!-- header end -->
 
-		<!-- banner start -->
-		<!-- ================ -->
-		<div id="banner" class="banner">
-			<div class="banner-image"></div>
-			<div class="banner-caption">
-				<div class="container">
-					<div class="row">
-						<div class="col-md-8 col-md-offset-2 object-non-visible" data-animation-effect="fadeIn">
-							<h1 class="text-center"></h1>
-							<p class="lead text-center"></p>
-						</div>
+	<!-- banner start -->
+	<!-- ================ -->
+	<div id="banner" class="banner">
+		<div class="banner-image"></div>
+		<div class="banner-caption">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-8 col-md-offset-2 object-non-visible"
+						data-animation-effect="fadeIn">
+						<h1 class="text-center"></h1>
+						<p class="lead text-center"></p>
 					</div>
 				</div>
 			</div>
 		</div>
-		<!-- banner end -->
+	</div>
+	<!-- banner end -->
+	<div class="container content">
+		<section id="about">Lorem ipsum dolor sit amet, consectetur adipiscing
+			elit. Praesent ac quam tortor. Vivamus luctus dui sed arcu volutpat,
+			vitae mattis neque viverra. Nulla facilisis nibh sit amet finibus
+			interdum. In sed ante justo. Duis et nibh ac nibh tristique convallis
+			ac vitae risus. Suspendisse tincidunt tortor suscipit nisl sagittis
+			volutpat. Suspendisse vestibulum risus ut orci interdum fermentum.
+			Nunc elementum id tellus vitae ultricies. Sed vitae leo tincidunt,
+			consectetur urna sed, viverra felis. Nam maximus venenatis justo,
+			quis viverra nisi vestibulum eget. Morbi sit amet lectus et massa
+			placerat porttitor. Donec a dui euismod nibh suscipit rutrum. Mauris
+			ultrices id nunc at venenatis. Nulla nibh tellus, euismod at purus a,
+			rhoncus dapibus urna. Quisque eget dictum nulla, eu ornare neque.
 
-		
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+			nec vestibulum justo. Aenean rhoncus faucibus posuere. Cras ut lorem
+			in mauris gravida lobortis. Nunc ut odio turpis. Nam finibus dictum
+			leo at tempus. In quis pulvinar nibh. Mauris egestas nibh sit amet
+			nulla vehicula, eget ullamcorper mi ultricies. Class aptent taciti
+			sociosqu ad litora torquent per conubia nostra, per inceptos
+			himenaeos. Proin semper, nisi sit amet tincidunt pretium, enim nisi
+			molestie dui, et luctus nunc enim ut ante. Vivamus porta, lacus id
+			sagittis sollicitudin, nibh nibh laoreet elit, fermentum placerat
+			diam ante tempor nisi. Suspendisse blandit a tortor eget blandit.
+			Morbi eu metus mollis, feugiat massa eget, elementum sem.
+			Pellentesque lacinia vestibulum nibh eget faucibus. Sed odio arcu,
+			volutpat nec tellus condimentum, dapibus laoreet nunc. Donec posuere
+			elit eu odio sollicitudin, rutrum porta quam hendrerit. In nec auctor
+			lectus, ut fermentum leo. Nunc vitae porttitor nibh. Donec a rhoncus
+			ligula. Donec aliquam faucibus velit, non efficitur quam sagittis eu.
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+			egestas ligula nec ante vestibulum, et semper ex aliquet. Duis
+			aliquet urna quis sapien congue, eget dictum odio accumsan. Integer
+			eget purus tincidunt, vestibulum nibh sed, sodales neque. Curabitur
+			in ultricies sapien, id blandit lacus. Maecenas ut ante malesuada
+			eros ultricies aliquam in ut purus. Sed nec sem ligula. Aenean
+			faucibus tempus leo, sed tristique nulla. Mauris dolor odio, pulvinar
+			a purus at, fermentum dapibus mi. Ut vel accumsan dui, sed tincidunt
+			velit. Class aptent taciti sociosqu ad litora torquent per conubia
+			nostra, per inceptos himenaeos. Suspendisse vitae viverra nunc.
+			Integer molestie arcu elit, id vulputate nisl lacinia a. Proin turpis
+			erat, sodales eu vehicula at, vestibulum vitae magna. Vestibulum elit
+			sem, cursus et vestibulum vitae, finibus eget tellus. Vivamus ut
+			accumsan massa. Nunc consequat felis a arcu sagittis lacinia.
+			Pellentesque in condimentum eros, eget maximus diam. Proin pulvinar
+			ex ipsum, sed ornare massa ultrices quis. Duis imperdiet, diam vel
+			vestibulum ultricies, ligula ligula pretium dolor, a iaculis urna
+			nisl et eros. Nullam mollis euismod velit, vitae lacinia purus
+			interdum sit amet. Aenean condimentum mauris vitae orci vestibulum
+			pellentesque. Phasellus congue sapien nec dolor consequat feugiat.
+			Phasellus sit amet massa aliquam, tristique nisl at, molestie augue.
+			Etiam eu ante ut nibh sodales aliquet quis non quam. Nunc ut justo
+			sed erat cursus vehicula.</section>
+	</div>
+	
+	<footer class="footer">
+	<div class="container">
+	<p class="text-muted">
+	Copyright &copy; Love Inc <?php echo date("Y"); ?>
+	</p>
+	</div>
+	</footer>
 
-		<!-- JavaScript files placed at the end of the document so the pages load faster
+	<!-- JavaScript files placed at the end of the document so the pages load faster
 		================================================== -->
-		<!-- Jquery and Bootstap core js files -->
-		<script type="text/javascript" src="plugins/jquery.min.js"></script>
-		<script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+	<!-- Jquery and Bootstap core js files -->
+	<script type="text/javascript" src="plugins/jquery.min.js"></script>
+	<script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
 
-		<!-- Modernizr javascript -->
-		<script type="text/javascript" src="plugins/modernizr.js"></script>
+	<!-- Modernizr javascript -->
+	<script type="text/javascript" src="plugins/modernizr.js"></script>
 
-		<!-- Isotope javascript -->
-		<script type="text/javascript" src="plugins/isotope/isotope.pkgd.min.js"></script>
-		
-		<!-- Backstretch javascript -->
-		<script type="text/javascript" src="plugins/jquery.backstretch.min.js"></script>
+	<!-- Isotope javascript -->
+	<script type="text/javascript"
+		src="plugins/isotope/isotope.pkgd.min.js"></script>
 
-		<!-- Appear javascript -->
-		<script type="text/javascript" src="plugins/jquery.appear.js"></script>
+	<!-- Backstretch javascript -->
+	<script type="text/javascript" src="plugins/jquery.backstretch.min.js"></script>
 
-		<!-- Initialization of Plugins -->
-		<script type="text/javascript" src="js/template.js"></script>
+	<!-- Appear javascript -->
+	<script type="text/javascript" src="plugins/jquery.appear.js"></script>
 
-		<!-- Custom Scripts -->
-		<script type="text/javascript" src="js/custom.js"></script>
+	<!-- Initialization of Plugins -->
+	<script type="text/javascript" src="js/template.js"></script>
 
-	</body>
+	<!-- Custom Scripts -->
+	<script type="text/javascript" src="js/custom.js"></script>
+
+</body>
 </html>
