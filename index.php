9599dddae3ccc63d25c8f7d46c78fca14f611f7c
Alex Johnson <mrjohnsonalex@gmail.com>
2015-03-18 13:43:49 -0400

    some housekeeping

--- index.php
+++ index.php
@@ -160,71 +160,73 @@
 <div class="container wrap">
     <section id="about">
         <h1 class="text-center">About Us</h1>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent ac
-        quam tortor. Vivamus luctus dui sed arcu volutpat, vitae mattis neque
-        viverra. Nulla facilisis nibh sit amet finibus interdum. In sed ante
-        justo. Duis et nibh ac nibh tristique convallis ac vitae risus.
-        Suspendisse tincidunt tortor suscipit nisl sagittis volutpat.
-        Suspendisse vestibulum risus ut orci interdum fermentum. Nunc
-        elementum id tellus vitae ultricies. Sed vitae leo tincidunt,
-        consectetur urna sed, viverra felis. Nam maximus venenatis justo,
-        quis viverra nisi vestibulum eget. Morbi sit amet lectus et massa
-        placerat porttitor. Donec a dui euismod nibh suscipit rutrum. Mauris
-        ultrices id nunc at venenatis. Nulla nibh tellus, euismod at purus a,
-        rhoncus dapibus urna. Quisque eget dictum nulla, eu ornare neque.
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
-        nec vestibulum justo. Aenean rhoncus faucibus posuere. Cras ut lorem
-        in mauris gravida lobortis. Nunc ut odio turpis. Nam finibus dictum
-        leo at tempus. In quis pulvinar nibh. Mauris egestas nibh sit amet
-        nulla vehicula, eget ullamcorper mi ultricies. Class aptent taciti
-        sociosqu ad litora torquent per conubia nostra, per inceptos
-        himenaeos. Proin semper, nisi sit amet tincidunt pretium, enim nisi
-        molestie dui, et luctus nunc enim ut ante. Vivamus porta, lacus id
-        sagittis sollicitudin, nibh nibh laoreet elit, fermentum placerat
-        diam ante tempor nisi. Suspendisse blandit a tortor eget blandit.
-        Morbi eu metus mollis, feugiat massa eget, elementum sem.
-        Pellentesque lacinia vestibulum nibh eget faucibus. Sed odio arcu,
-        volutpat nec tellus condimentum, dapibus laoreet nunc. Donec posuere
-        elit eu odio sollicitudin, rutrum porta quam hendrerit. In nec auctor
-        lectus, ut fermentum leo. Nunc vitae porttitor nibh. Donec a rhoncus
-        ligula. Donec aliquam faucibus velit, non efficitur quam sagittis eu.
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
-        egestas ligula nec ante vestibulum, et semper ex aliquet. Duis
-        aliquet urna quis sapien congue, eget dictum odio accumsan. Integer
-        eget purus tincidunt, vestibulum nibh sed, sodales neque. Curabitur
-        in ultricies sapien, id blandit lacus. Maecenas ut ante malesuada
-        eros ultricies aliquam in ut purus. Sed nec sem ligula. Aenean
-        faucibus tempus leo, sed tristique nulla. Mauris dolor odio, pulvinar
-        a purus at, fermentum dapibus mi. Ut vel accumsan dui, sed tincidunt
-        velit. Class aptent taciti sociosqu ad litora torquent per conubia
-        nostra, per inceptos himenaeos. Suspendisse vitae viverra nunc.
-        Integer molestie arcu elit, id vulputate nisl lacinia a. Proin turpis
-        erat, sodales eu vehicula at, vestibulum vitae magna. Vestibulum elit
-        sem, cursus et vestibulum vitae, finibus eget tellus. Vivamus ut
-        accumsan massa. Nunc consequat felis a arcu sagittis lacinia.
-        Pellentesque in condimentum eros, eget maximus diam. Proin pulvinar
-        ex ipsum, sed ornare massa ultrices quis. Duis imperdiet, diam vel
-        vestibulum ultricies, ligula ligula pretium dolor, a iaculis urna
-        nisl et eros. Nullam mollis euismod velit, vitae lacinia purus
-        interdum sit amet. Aenean condimentum mauris vitae orci vestibulum
-        pellentesque. Phasellus congue sapien nec dolor consequat feugiat.
-        Phasellus sit amet massa aliquam, tristique nisl at, molestie augue.
-        Etiam eu ante ut nibh sodales aliquet quis non quam. Nunc ut justo
-        sed erat cursus vehicula.
+
+        <div class="row">
+            <div class="col-md-8">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent ac
+                quam tortor. Vivamus luctus dui sed arcu volutpat, vitae mattis neque
+                viverra. Nulla facilisis nibh sit amet finibus interdum. In sed ante
+                justo. Duis et nibh ac nibh tristique convallis ac vitae risus.
+                Suspendisse tincidunt tortor suscipit nisl sagittis volutpat.
+                Suspendisse vestibulum risus ut orci interdum fermentum. Nunc
+                elementum id tellus vitae ultricies. Sed vitae leo tincidunt,
+                consectetur urna sed, viverra felis. Nam maximus venenatis justo,
+                quis viverra nisi vestibulum eget. Morbi sit amet lectus et massa
+                placerat porttitor. Donec a dui euismod nibh suscipit rutrum. Mauris
+                ultrices id nunc at venenatis. Nulla nibh tellus, euismod at purus a,
+                rhoncus dapibus urna. Quisque eget dictum nulla, eu ornare neque.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+                nec vestibulum justo. Aenean rhoncus faucibus posuere. Cras ut lorem
+                in mauris gravida lobortis. Nunc ut odio turpis. Nam finibus dictum
+                leo at tempus. In quis pulvinar nibh. Mauris egestas nibh sit amet
+                nulla vehicula, eget ullamcorper mi ultricies. Class aptent taciti
+                sociosqu ad litora torquent per conubia nostra, per inceptos
+                himenaeos. Proin semper, nisi sit amet tincidunt pretium, enim nisi
+                molestie dui, et luctus nunc enim ut ante. Vivamus porta, lacus id
+                sagittis sollicitudin, nibh nibh laoreet elit, fermentum placerat
+                diam ante tempor nisi. Suspendisse blandit a tortor eget blandit.
+                Morbi eu metus mollis, feugiat massa eget, elementum sem.
+                Pellentesque lacinia vestibulum nibh eget faucibus. Sed odio arcu,
+                volutpat nec tellus condimentum, dapibus laoreet nunc. Donec posuere
+                elit eu odio sollicitudin, rutrum porta quam hendrerit. In nec auctor
+                lectus, ut fermentum leo. Nunc vitae porttitor nibh. Donec a rhoncus
+                ligula. Donec aliquam faucibus velit, non efficitur quam sagittis eu.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+                egestas ligula nec ante vestibulum, et semper ex aliquet. Duis
+                aliquet urna quis sapien congue, eget dictum odio accumsan. Integer
+                eget purus tincidunt, vestibulum nibh sed, sodales neque. Curabitur
+                in ultricies sapien, id blandit lacus. Maecenas ut ante malesuada
+                eros ultricies aliquam in ut purus. Sed nec sem ligula. Aenean
+                faucibus tempus leo, sed tristique nulla. Mauris dolor odio, pulvinar
+                a purus at, fermentum dapibus mi. Ut vel accumsan dui, sed tincidunt
+                velit. Class aptent taciti sociosqu ad litora torquent per conubia
+                nostra, per inceptos himenaeos. Suspendisse vitae viverra nunc.
+                Integer molestie arcu elit, id vulputate nisl lacinia a. Proin turpis
+                erat, sodales eu vehicula at, vestibulum vitae magna. Vestibulum elit
+                sem, cursus et vestibulum vitae, finibus eget tellus. Vivamus ut
+                accumsan massa. Nunc consequat felis a arcu sagittis lacinia.
+                Pellentesque in condimentum eros, eget maximus diam. Proin pulvinar
+                ex ipsum, sed ornare massa ultrices quis. Duis imperdiet, diam vel
+                vestibulum ultricies, ligula ligula pretium dolor, a iaculis urna
+                nisl et eros. Nullam mollis euismod velit, vitae lacinia purus
+                interdum sit amet. Aenean condimentum mauris vitae orci vestibulum
+                pellentesque. Phasellus congue sapien nec dolor consequat feugiat.
+                Phasellus sit amet massa aliquam, tristique nisl at, molestie augue.
+                Etiam eu ante ut nibh sodales aliquet quis non quam. Nunc ut justo
+                sed erat cursus vehicula.
+            </div>
+            <div class="col-md-4">
+                <div class="fb-like-box" data-href="https://www.facebook.com/loveofjackson" data-colorscheme="light"
+                     data-show-faces="true" data-header="true" data-stream="true" data-show-border="true"></div>
+            </div>
+        </div>
     </section>
 </div>
 <footer class="footer">
     <div class="container">
-        <div class="row">
-            <div class="col-md-8">
-                <p class="text-muted">
-                    Copyright &copy; Love Inc <?php echo date("Y"); ?>
-                </p>
-            </div>
-            <div class="col-md-4">
-
-            </div>
-        </div>
+        <p class="text-muted">
+            Copyright &copy; <?php echo date("Y"); ?> Love Inc of Jackson
+        </p>
     </div>
 </footer>
 
