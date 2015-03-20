bdc365c9acbc8844b6ce8bd81b202adaf500940f
Alex Johnson <mrjohnsonalex@gmail.com>
2015-03-19 20:30:47 -0400

    added a pic

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,6 @@
 $(".banner-image").backstretch(['images/love_banner.jpg',
-    'images/mission.jpg'],
+    'images/mission.jpg',
+    'images/volunteers&patton.jpg'],
     {
         duration:3000, fade: 750
     }
